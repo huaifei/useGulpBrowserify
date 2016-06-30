e4ac0d5879540998b97d16b806907d9b6534c37d
Huaifei Zhang <huaifei.zhang@accenture.com>
2016-06-30 18:25:24 +0800

    Felix

--- gulpfile.js
+++ gulpfile.js
@@ -17,24 +17,69 @@ var gulp = require('gulp'),
 //         .pipe(gulp.dest('./'));
 // });
 
-gulp.task('build', function() {
-    return buildScript('./app/js/main.js', false);
+
+//-----------------------------------------------
+
+var global = {
+    isWatching: true
+};
+
+gulp.task('watch',function () {
+    var bundler = browserify('./app/js/main.js');
+    // var watchedfiles = browserify('./app/js/*.js');
+    
+    var bundleFunc = function () {
+        return bundler.bundle()
+            .on('error', handleErrors)
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('./'));
+    };
+
+    if(global.isWatching) {
+        bundler = watchify(bundler);
+        bundler.on('update', bundleFunc);
+    }
+
+    return bundleFunc();
+
 });
 
-gulp.task('default', ['build'], function() {
-    return buildScript('./app/js/main.js', true);
+
+gulp.task('default',function () {
+    gulp.start('watch');
 });
+
+// gulp.task('browserify', watchify(function(watchify) {
+//     return gulp.src(bundlePaths.src)
+//         .pipe(watchify({
+//             watch:watching
+//         }))
+//         .pipe(gulp.dest(bundlePaths.dest))
+// }));
+
+
+//-----------------------------------------------
+
+
+// gulp.task('build', function() {
+//     return buildScript('./app/js/main.js', false);
+// });
+//
+// gulp.task('default', ['build'], function() {
+//     return buildScript('./app/js/main.js', true);
+// });
+
 
 function buildScript(file, watch) {
     var props = {entries: [file]};
     var bundler = watch ? watchify(props) : browserify(props);
-    bundler.transform(reactify);
+    // bundler.transform(reactify);
 
     function rebundle() {
-        var stream = bundler.bundle({debug: true});
+        var stream = bundler.bundle();
         return stream.on('error', handleErrors)
-            .pipe(source(file))
-            .pipe(gulp.dest(buildDir + '/'));
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('./'));
     }
 
     bundler.on('update', function() {
@@ -54,6 +99,8 @@ function handleErrors() {
     this.emit('end');
     // Keep gulp from hanging on this task
 }
+
+//-----------------------------------------------
 
 // gulp.task('stream', function (){
 //     return watch('./app/**/*.*',bundle);
